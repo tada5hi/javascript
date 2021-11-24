8e473d0a786faf12ca8004939c231679790059bb
Peter <peter.placzek1996@gmail.com>
2021-11-24 09:28:43 +0100

    added dependabot config + prettifyier package + airbnb style guide

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,11 +1,13 @@
 module.exports =  {
     extends:  [
-        'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-        'plugin:import/typescript'
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
     ],
     parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
     parserOptions:  {
-        ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+        ecmaVersion:  2020,  // Allows for the parsing of modern ECMAScript features
         sourceType:  'module',  // Allows for the use of imports
     },
     plugins: [
