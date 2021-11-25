598a9c42ae1ec30dcf9a264eac247216e07aef31
Peter <peter.placzek1996@gmail.com>
2021-11-25 13:11:32 +0100

    fixed vue3 config

--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -1,7 +1,3 @@
-const { rules: baseStyleRules } = require('eslint-plugin-vue/lib');
-
-baseStyleRules['html-indent'][1] = 4;
-
 module.exports = {
     extends:  [
         'plugin:vue/vue3-recommended',
@@ -11,7 +7,6 @@ module.exports = {
         'vue'
     ],
     rules: {
-        "vue/multi-word-component-names": "off",
-        "vue/html-indent": baseStyleRules['html-indent']
+        "vue/multi-word-component-names": "off"
     }
 };
