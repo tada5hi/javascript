5d90c9f3257c9226f4371fbbc40bdc579acd1b87
Peter <peter.placzek1996@gmail.com>
2021-11-25 17:48:42 +0100

    updated eslint-vue-typescript package

--- packages/eslint-config-vue-typescript/index.js
+++ packages/eslint-config-vue-typescript/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     extends:  [
-        '@tada5hi/eslint-config-vue',
-        '@tada5hi/eslint-config-typescript'
+        '@tada5hi/eslint-config-vue'
     ],
+    plugins: ['@typescript-eslint'],
     parser: 'vue-eslint-parser',
     parserOptions: {
         parser: '@typescript-eslint/parser'
