d6ff27b90ccfe1a9f87ff819a582fce94ff7ed68
Peter <peter.placzek1996@gmail.com>
2021-11-25 13:14:40 +0100

    set vue html indent rule

--- packages/eslint-config-vue3/index.js
+++ packages/eslint-config-vue3/index.js
@@ -7,6 +7,13 @@ module.exports = {
         'vue'
     ],
     rules: {
-        "vue/multi-word-component-names": "off"
+        "vue/multi-word-component-names": "off",
+        "vue/html-indent": ["error", 4, {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }]
     }
 };
