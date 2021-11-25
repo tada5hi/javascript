607eb760e176eb55cc35cf5585e154dc785526a8
Peter <peter.placzek1996@gmail.com>
2021-11-25 17:19:33 +0100

    modified parseOptions of config-vue-typescript

--- packages/eslint-config-vue-typescript/index.js
+++ packages/eslint-config-vue-typescript/index.js
@@ -3,5 +3,8 @@ module.exports = {
         '@tada5hi/eslint-config-vue',
         '@tada5hi/eslint-config-typescript'
     ],
-    parser: 'vue-eslint-parser'
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    }
 };
