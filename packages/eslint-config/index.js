99b80848986eef9e0e7123561141e5b638d6b92a
Peter <peter.placzek1996@gmail.com>
2021-11-25 14:25:20 +0100

    reset eslint base

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -14,9 +14,6 @@ module.exports = {
         ecmaVersion:  2020,  // Allows for the parsing of modern ECMAScript features
         sourceType:  'module',  // Allows for the use of imports
     },
-    plugins: [
-        "import"
-    ],
     rules:  {
         "quotes": ["error", "single"],
         "sort-imports": ["error", {
