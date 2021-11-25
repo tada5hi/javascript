873521179bb015de87b8232acfcab60bd6ad8d5b
Peter <peter.placzek1996@gmail.com>
2021-11-25 18:04:20 +0100

    updated vue rules

--- packages/eslint-config-vue-typescript/index.js
+++ packages/eslint-config-vue-typescript/index.js
@@ -6,5 +6,15 @@ module.exports = {
     parser: 'vue-eslint-parser',
     parserOptions: {
         parser: '@typescript-eslint/parser'
+    },
+    rules: {
+        "vue/multi-word-component-names": "off",
+        "vue/html-indent": ["error", 4, {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }]
     }
 };
